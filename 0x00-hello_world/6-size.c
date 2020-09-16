7ac510faf68ce2897bff0b2d69a4a5ffc8916d67
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-16 20:02:56 +0000

    Size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
-
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
