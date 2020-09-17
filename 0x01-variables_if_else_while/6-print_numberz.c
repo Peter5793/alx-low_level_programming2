2b08e7dfb355639ca8d3b427be5c2b36af10d1e6
Peter Lugalia <p.lugalia@launcpad.alx.app>
2020-09-17 18:28:07 +0000

    Using Putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry print single digit numbers to base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <  10 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
